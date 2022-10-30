c2182fe4ba8978c9ab75e3b3da6dfea7d5eeb526
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2022-10-30 08:36:21 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ const enviarFormulario = () => {
     let numero = document.getElementById('numero').value;
 
 
-    var win = window.open(`https://wa.me/54${numero}?text=App:%20 
-https://bit.ly/3gAGP0I`, '_blank');
+    var win = window.open(`https://wa.me/54${numero}?text=Mis Fotos App Web:%20https://bit.ly/3gAGP0I%0APara que el enlace se active me tienes que enajenar`, '_blank');
     //
 }
 evento.addEventListener('click', enviarFormulario)
+
+
+//Compartir
+const shareBtn = document.getElementById('shareBtn')
+shareBtn.addEventListener('click', event => {
+    if (navigator.share) {
+        //drive
+        navigator.share({
+            text: 'Por favor, lea este gran artículo: ',
+            url: 'https://bit.ly/3gAGP0I'
+        }).then(() => {
+            console.log('Gracias por compartir mis fotos!');
+        })
+            .catch((err) => console.error(err));
+    } else {
+
+        alert("El navegador actual no admite la función de compartir. Por favor, comparte manualmente el enlace")
+    }
+});
+
 
 //Numero de pagina
 function modal_009() {
@@ -32,30 +51,97 @@ function modal_009() {
     })
 }
 
-//Compartir
-const shareBtn = document.getElementById('shareBtn')
-shareBtn.addEventListener('click', event => {
-    if (navigator.share) {
-        //drive
-        navigator.share({
-            text: 'Please read this great article: ',
-            url: 'https://bit.ly/3gAGP0I'
-        }).then(() => {
-            console.log('Thanks for sharing!');
-        })
-            .catch((err) => console.error(err));
-    } else {
 
-        alert("The current browser does not support the share function. Please, manually share the link")
+// automatica
+Swal.fire({
+
+    html: '<div class="mensaje-2">' +
+        '<h3 class="bm-4">Black Momets</h3>' +
+        '<p class="bm-2-pp">Foto disponible el <b>4 de octubre</b>, Si no aparecen escribeme al </p>' +
+        '<a href="https://wa.me/541144382987" target="_blank"><button class="bt-msj" >+54 11 4438-2987</button></a>' +
+        '</div>',
+   
+       html: '<div class="mensaje">' +
+       '<h3 class="bm-4">Black Momets</h3>' +
+       '<p class="bm-2-pp">Fotos listas, gracias por la confianza</p>' +
+       '</div>',
+   
+    //tamaño
+    imageWidth: 300,
+    imageHeight: 300,
+    //cerrar
+    padding: '1rem',
+    showCloseButton: true,
+    showConfirmButton: false,
+    background: 'linear-gradient(#000,#000)',
+    //ajustes
+    showCloseButton: true,
+    timer: '3000',
+    timerProgressBar: true,
+    customClass: {
+        actions: 'content',
     }
+})
+
+
+//Abrir fotos automaticamante
+let boton = document.querySelector('#boton');
+boton.addEventListener('click', function () {
+
+    setTimeout(function () {
+        location.href = "https://drive.google.com/drive/folders/1i2O5zPdlkUWG-3gJilWHg-Uude29nrNZ?usp=sharing"
+    }, 1000 * 3)
+
+
+
 });
 
 
+//ventana automaticamante
+function modal_08() {
+    Swal.fire({
+        html: '<div class="mensaje">' +
+            '<h3 class="bm-2-h3">Black Momets</h3>' +
+            '<p class="bm-2-p">Tus Fotos Al Mejor Precio. </p>' +
+            '</div>',
+        //tamaño
+        imageWidth: 400,
+        imageHeight: 500,
+        //cerrar
+        padding: '1rem',
+        showCloseButton: true,
+        showConfirmButton: false,
+        background: '#000',
+        //ajustes
+        showCloseButton: true,
+        timer: '5000',
+        timerProgressBar: true,
+        timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
+}
 
 
+
+// Foto lhuis ruiz
 function modal_05() {
     Swal.fire({
-        html: '<h3  onclick="modal_009();" class="bm">Black Momets</h3>',
+        html: '<h3>Black Momets</h3>'+
+        '<p>Fotos al mejor precio.</p>',
         imageUrl: 'https://res.cloudinary.com/lhuis/image/upload/v1666754485/iconos/fkppmxoe9qs9xhggmzcc.png',
         //tamaño
         imageWidth: 300,
@@ -69,36 +155,4 @@ function modal_05() {
     })
 }
 
-// automatica
-setTimeout(function () {
-Swal.fire({
-    
-    html: '<div class="mensaje">' +
-        '<h3 class="bm">Black Momets</h3>' +
-        '<p>Foto disponible el <b>4 de octubre</b>, Si no aparecen escribeme al </p>' +
-        '<a href="https://wa.me/541144382987" target="_blank"><button class="bt-msj" >+54 11 4438-2987</button></a>' +
-        '</div>',
-     /*
-        html: '<div class="mensaje">' +
-        '<h3 class="bm">Black Momets</h3>' +
-        '<p>Fotos listas, gracias por la confianza</p>' +
-        
-        '</div>',
-    */
-    //tamaño
-    imageWidth: 300,
-    imageHeight: 300,
-    //cerrar
-    padding: '1rem',
-    showCloseButton: true,
-    showConfirmButton: false,
-    background: 'linear-gradient(#000,#000)',
-    //ajustes
-    showCloseButton: true,
-    timer: '5000',
-    timerProgressBar: true,
-    customClass: {
-        actions: 'content',
-    }
-})
-}, 1000 * 2);
+
